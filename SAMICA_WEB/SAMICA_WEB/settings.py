0e5eb68fd5749d83bf6f6cad513b577d73c4f0e8
TaponTV <tapiavazquez.l1518@gmail.com>
2024-05-06 22:53:38 -0600

    model studentgraduate progress

--- SAMICA_WEB/SAMICA_WEB/settings.py
+++ SAMICA_WEB/SAMICA_WEB/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'SAMICA_WEB.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'samica_database',
+        'USER': 'postgres',
+        'PASSWORD': 'samica',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
--- SAMICA_WEB/apps/graduates/models/StudentGraduate.py
+++ SAMICA_WEB/apps/graduates/models/StudentGraduate.py
@@ -1,7 +1,9 @@
 from django.db import models as md
 
-
 class StudentGraduate(md.Model):
+    """
+    Esta clase pertenece al modelo de la tabla de estudiantes graduados dentro del sistema
+    """
     class Meta:
         db_table = 'student_graduate'
 
