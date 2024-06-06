6a5385998958fcfb43ee4be97ce6c2897a0b4292
TaponTV <tapiavazquez.l1518@gmail.com>
2024-06-06 15:19:14 -0600

    not important commit

--- SAMICA_WEB/apps/dashboard/views/dashboard_view.py
+++ SAMICA_WEB/apps/dashboard/views/dashboard_view.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from utils.get_user_data import get_user_data
 
+
+
 @login_required
 def DashboardHomeView(request):
     auth_user = request.user
